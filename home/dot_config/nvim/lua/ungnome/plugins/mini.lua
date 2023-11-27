1f57f3f25f637153041f02de976c4bf33d4b8d54
ungnome <73322863+ungnome@users.noreply.github.com>
2023-11-27 14:13:02 -0500

    fix(nvim): missing comment string for terraform/hcl files

--- home/dot_config/nvim/lua/ungnome/plugins/mini.lua
+++ home/dot_config/nvim/lua/ungnome/plugins/mini.lua
@@ -18,6 +18,15 @@ return {
       },
     })
 
+    -- fix terraform and hcl comment string
+    vim.api.nvim_create_autocmd("FileType", {
+      group = vim.api.nvim_create_augroup("FixTerraformCommentString", { clear = true }),
+      callback = function(ev)
+        vim.bo[ev.buf].commentstring = "# %s"
+      end,
+      pattern = { "terraform", "hcl" },
+    })
+
     require("mini.pairs").setup()
 
     require("mini.indentscope").setup({
