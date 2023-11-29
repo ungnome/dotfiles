e002501dbd6db09d28036d8c6bbe3eb3e721fe7e
ungnome <73322863+ungnome@users.noreply.github.com>
2023-11-29 14:49:42 -0500

    fix(neovim): auto-dark-mode error message

--- home/dot_config/nvim/lua/ungnome/plugins/auto-dark-mode.lua
+++ home/dot_config/nvim/lua/ungnome/plugins/auto-dark-mode.lua
@@ -8,7 +8,7 @@ return {
     update_interval = 1000,
     set_dark_mode = function()
       vim.api.nvim_set_option("background", "dark")
-      vim.cmd("colorscheme tokyonight-night")
+      vim.cmd("colorscheme catppuccin-mocha")
     end,
     set_light_mode = function()
       vim.api.nvim_set_option("background", "light")
