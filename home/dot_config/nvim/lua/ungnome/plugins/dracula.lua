ea5466561fc9089952de4fec7faafa70752e1757
ungnome <73322863+ungnome@users.noreply.github.com>
2024-03-16 22:12:32 -0400

    adjust terminal and vim bg color

--- home/dot_config/nvim/lua/ungnome/plugins/dracula.lua
+++ home/dot_config/nvim/lua/ungnome/plugins/dracula.lua
@@ -2,13 +2,19 @@ return {
 	lazy = false,
 	enabled = true,
 	"Mofiqul/dracula.nvim",
-	opts = {
-		colors = {
-			bg = "#121212",
-		},
-	},
-	config = function(_, opts)
-		require("dracula").setup(opts)
+	config = function()
+		local colors = require("dracula").colors()
+
+		require("dracula").setup({
+			colors = {
+				bg = "#23252F",
+			},
+			show_end_of_buffer = true,
+			overrides = {
+				EndOfBuffer = { fg = colors.purple },
+			},
+		})
+
 		vim.cmd.colorscheme("dracula")
 	end,
 }
