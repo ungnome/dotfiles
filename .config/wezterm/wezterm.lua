59e83e63c54db435ac042bbaa89bf9be96d83c08
ungnome <73322863+ungnome@users.noreply.github.com>
2024-08-20 17:41:37 -0400

    more tweaks

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,16 +3,14 @@ local config = wezterm.config_builder()
 
 local function scheme_for_appearance(appearance)
 	if appearance:find("Dark") then
-		os.execute("tmux set -g @catppuccin_flavour 'mocha' &> /dev/null; tmux source-file ~/.tmux.conf &> /dev/null")
 		return "Catppuccin Mocha"
 	else
-		os.execute("tmux set -g @catppuccin_flavour 'latte' &> /dev/null; tmux source-file ~/.tmux.conf &> /dev/null")
 		return "Catppuccin Latte"
 	end
 end
 
--- config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
-config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+-- config.color_scheme = "Catppuccin Mocha"
 config.scrollback_lines = 10000
 config.audible_bell = "Disabled"
 config.initial_rows = 40
