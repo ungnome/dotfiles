c7a64062d88b3d523d9701243b4b6a73bef45c0f
ungnome <73322863+ungnome@users.noreply.github.com>
2024-10-26 19:19:43 -0400

    feat(wezterm): tmuxish keybinds

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -17,11 +17,136 @@ config.initial_cols = 140
 config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
 config.native_macos_fullscreen_mode = true
+config.font = wezterm.font("Hack")
 config.font_size = 16.0
-config.font = wezterm.font({
-	family = "JetBrains Mono",
-	weight = "Medium",
-	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
-})
+
+-- keybinds
+config.leader = { key = "a", mods = "CTRL", timeou_milliseconds = 1000 }
+config.keys = {
+	-- split pane
+	{ key = "%", mods = "LEADER", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = '"', mods = "LEADER", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+
+	-- zoom pane
+	{ key = "m", mods = "LEADER", action = wezterm.action.TogglePaneZoomState },
+
+	-- workspaces
+	{
+		key = "w",
+		mods = "LEADER",
+		action = wezterm.action.PromptInputLine({
+			description = wezterm.format({
+				{ Attribute = { Intensity = "Bold" } },
+				{ Foreground = { AnsiColor = "Fuchsia" } },
+				{ Text = "Enter name for new workspace" },
+			}),
+			action = wezterm.action_callback(function(window, pane, line)
+				-- line will be `nil` if they hit escape without entering anything
+				-- An empty string if they just hit enter
+				-- Or the actual line of text they wrote
+				if line then
+					window:perform_action(
+						wezterm.action.SwitchToWorkspace({
+							name = line,
+						}),
+						pane
+					)
+				end
+			end),
+		}),
+	},
+	{
+		key = "s",
+		mods = "LEADER",
+		action = wezterm.action.ShowLauncherArgs({
+			flags = "FUZZY|WORKSPACES",
+		}),
+	},
+
+	-- panes
+	{
+		key = "r",
+		mods = "LEADER",
+		action = wezterm.action.ActivateKeyTable({
+			name = "resize_pane",
+			one_shot = false,
+			timeout_milliseconds = 1000,
+		}),
+	},
+	{
+		key = "p",
+		mods = "LEADER",
+		action = wezterm.action.ActivateKeyTable({
+			name = "switch_pane",
+			one_shot = false,
+			timeout_milliseconds = 1000,
+		}),
+	},
+
+	-- tabs
+	{
+		key = ",",
+		mods = "LEADER",
+		action = wezterm.action.PromptInputLine({
+			description = wezterm.format({
+				{ Attribute = { Intensity = "Bold" } },
+				{ Foreground = { AnsiColor = "Fuchsia" } },
+				{ Text = "Enter new name for tab" },
+			}),
+			action = wezterm.action_callback(function(window, pane, line)
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
+
+	-- copy mode
+	{
+		key = "[",
+		mods = "LEADER",
+		action = wezterm.action.ActivateCopyMode,
+	},
+}
+
+config.key_tables = {
+	resize_pane = {
+		{ key = "LeftArrow", action = wezterm.action.AdjustPaneSize({ "Left", 1 }) },
+		{ key = "h", action = wezterm.action.AdjustPaneSize({ "Left", 1 }) },
+		{ key = "RightArrow", action = wezterm.action.AdjustPaneSize({ "Right", 1 }) },
+		{ key = "l", action = wezterm.action.AdjustPaneSize({ "Right", 1 }) },
+		{ key = "UpArrow", action = wezterm.action.AdjustPaneSize({ "Up", 1 }) },
+		{ key = "k", action = wezterm.action.AdjustPaneSize({ "Up", 1 }) },
+		{ key = "DownArrow", action = wezterm.action.AdjustPaneSize({ "Down", 1 }) },
+		{ key = "j", action = wezterm.action.AdjustPaneSize({ "Down", 1 }) },
+		-- Cancel the mode by pressing escape
+		{ key = "y", action = "PopKeyTable" },
+	},
+	switch_pane = {
+		{ key = "LeftArrow", action = wezterm.action.ActivatePaneDirection("Left") },
+		{ key = "h", action = wezterm.action.ActivatePaneDirection("Left") },
+		{ key = "RightArrow", action = wezterm.action.ActivatePaneDirection("Right") },
+		{ key = "l", action = wezterm.action.ActivatePaneDirection("Right") },
+		{ key = "UpArrow", action = wezterm.action.ActivatePaneDirection("Up") },
+		{ key = "k", action = wezterm.action.ActivatePaneDirection("Up") },
+		{ key = "DownArrow", action = wezterm.action.ActivatePaneDirection("Down") },
+		{ key = "j", action = wezterm.action.ActivatePaneDirection("Down") },
+		-- Cancel the mode by pressing escape
+		{ key = "y", action = "PopKeyTable" },
+	},
+}
+
+-- event handlers
+wezterm.on("format-window-title", function(tab, pane, tabs, panes, config)
+	local zoomed = ""
+
+	if tab.active_pane.is_zoomed then
+		zoomed = "[Z]"
+	end
+
+	local workspace_name = wezterm.mux.get_active_workspace()
+
+	return "Workspace: " .. workspace_name .. " " .. zoomed
+end)
 
 return config
